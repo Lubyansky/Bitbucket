f3f4bacb3b98cc72091b0024f87ff1b4af36e610
Lubyansky <noobsa87@gmail.com>
2023-09-05 20:32:26 +0500

    Доработка валидации

--- Models/Account/RegisterModel.cs
+++ Models/Account/RegisterModel.cs
@@ -4,6 +4,7 @@ namespace Bitbucket.Models.Account
 {
     public class RegisterModel : BaseAuthModel
     {
+        [Required(ErrorMessage = "Не указан повтор пароля")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Пароль введен неверно")]
         public string ConfirmPassword { get; set; }
